dd490b688d0f7c0dd0aaa5278bc0c78efa75bc3f
Sam54123 <thesam54123@gmail.com>
2023-02-28 18:22:45 -0800

    boilerplate

---
+++ src/client/java/com/igrium/imm_pgun/ImmersivePortalGunClient.java
@@ -0,0 +1,10 @@
+package com.igrium.imm_pgun;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmersivePortalGunClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
